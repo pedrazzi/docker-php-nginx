076a1b45133eb12bdf2a044ea035be8c3de7dfd5
Pedrazzi <pedrazzi@hotmail.com>
2022-10-06 17:52:25 -0400

    Update .gitignore

---
+++ .config/docker-compose/php/php.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker run app php "$@"
+return $?
---
+++ vendor/composer/installed.php
@@ -0,0 +1,12 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pedrazzi/ibge-api',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '60cedb82a0806a109aa9cbb3f1e641b65efd70ae',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    )
+);
